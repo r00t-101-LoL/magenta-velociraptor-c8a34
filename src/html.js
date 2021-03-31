a0ac247bf4749f0977956d2973a93849abe5e0b5
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 00:46:56 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -19,6 +19,7 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
+        
       </body>
     </html>
   )
