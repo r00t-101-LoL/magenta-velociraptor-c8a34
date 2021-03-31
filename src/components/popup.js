a1b25bdc767230db275f5fb151b32bbf5b431714
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 00:40:42 +0000

    popup.js: changed

--- src/components/popup.js
+++ src/components/popup.js
@@ -10,6 +10,8 @@ function App() {
       <h1>Check if Helmet works as expected with script tags</h1>
       <p>Check console output - you will see $ is undefined</p>
       <Helmet>
+
+      
         <script
           src="https://code.jquery.com/jquery-3.3.1.min.js"
           integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
