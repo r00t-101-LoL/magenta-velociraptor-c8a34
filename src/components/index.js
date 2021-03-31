8d868169b213b37595ce365437a170596a0f128d
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 02:12:39 +0000

    index.js: changed

--- src/components/index.js
+++ src/components/index.js
@@ -14,7 +14,7 @@ import HeroSection from './HeroSection';
 import SectionActions from './SectionActions';
 import TeamSection from './TeamSection';
 import Layout from './Layout';
-import Pop from './Pop'
+import Pop from './Pop';
 
 export {
     Action,
