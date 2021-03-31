9f37f784c76c7112c31b4623eddfe57746891a86
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 02:15:51 +0000

    Pop.js: changed

--- src/components/Pop.js
+++ src/components/Pop.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Helmet } from "react-helmet";
 
-import "./styles.css";
-
 function Pop() {
   return (
     <div className="pop">
