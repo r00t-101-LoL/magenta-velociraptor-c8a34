67f97dd0e9fbfb1ba2fec6fca7582ec91a4838c4
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 00:39:49 +0000

    popup.js: changed

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,2 +1,29 @@
-import * as React from 'react'
-import {Helmet} from 'react-helmet'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Check if Helmet works as expected with script tags</h1>
+      <p>Check console output - you will see $ is undefined</p>
+      <Helmet>
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.min.js"
+          integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+          crossorigin="anonymous"
+        />
+        <script>
+          {`
+            console.log('Test', typeof $);
+          `}
+        </script>
+      </Helmet>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
