3af73194b392ca5f2b7ac713fce25f855db2b8b0
Stackbit Code Editor <projects@stackbit.com>
2021-03-31 00:46:00 +0000

    Pop.js: added

---
+++ src/components/Pop.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Helmet } from "react-helmet";
+
+import "./styles.css";
+
+function Pop() {
+  return (
+    <div className="pop">
+      <Helmet>
+        <script 
+          type="text/javascript" 
+          src="https://apiv2.popupsmart.com/api/Bundle/363398" async />
+        <script>
+          {`
+            console.log('Test', typeof $);
+          `}
+        </script>
+      </Helmet>
+    </div>
+  );
+}
+
+export default Pop;
