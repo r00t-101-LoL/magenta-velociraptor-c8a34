c4af94f78eee790df58f98ac5e1426e7cda6e96a
Stackbit Code Editor <projects@stackbit.com>
2021-04-01 19:35:02 +0000

    gatsby-config.js: changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,23 @@ module.exports = {
             options: {}
         },
 
-        
+        {
+            resolve: 'gatsby-plugin-load-script',
+           /* options: {
+              disable: !process.env.SENTRY_DSN, // When do you want to disable it ?
+              src: 'https://browser.sentry-cdn.com/5.15.4/bundle.min.js',
+              integrity:
+                'sha384-Nrg+xiw+qRl3grVrxJtWazjeZmUwoSt0FAVsbthlJ5OMpx0G08bqIq3b/v0hPjhB',
+              crossorigin: 'anonymous',
+              onLoad: `() => Sentry.init({dsn:"${process.env.SENTRY_DSN}"})`,
+            },
+          },
+        ], */
+        options: {
+            src: '/assets/scripts/test.js', // Change to the script filename
+          },
+      },
+
         {
             resolve: `@stackbit/gatsby-plugin-menus`,
             options: {
