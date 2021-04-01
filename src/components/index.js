df13240a56e80a365939650ab58f1fc23378bd5b
Stackbit Code Editor <projects@stackbit.com>
2021-04-01 18:10:31 +0000

    index.js: changed

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ import BlogPostTags from './BlogPostTags';
 import ContactSection from './ContactSection';
 import ContentSection from './ContentSection';
 import CtaSection from './CtaSection';
-import Relax from './rela';
+import FeaturesSection from './FeaturesSection';
 import Footer from './Footer';
 import Header from './Header';
 import HeroSection from './HeroSection';
